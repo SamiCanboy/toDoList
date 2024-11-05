811c8da078a747decf7b3a932f06cd1b6e3f01bb
Sami Canboy <68285862+SamiCanboy@users.noreply.github.com>
2024-11-06 01:43:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function olustur() {
+    let baslik = document.getElementById('input-title')
+    let icerik = document.getElementById('input-content')
+    if (baslik.length = null) {
+        alert('Başlık boş olamaz');
+        console.log('Baslik')
+    }
+    else if(icerik.length = null)
+    {
+        alert('İçerik boş olamaz');
+    }
+}
